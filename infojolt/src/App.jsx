fdeba38f771d3088cf5b9c23576596e79422526c
Anshit <anshit12005@gmail.com>
2026-02-28 13:18:42 +0530

    commit

--- infojolt/src/App.jsx
+++ infojolt/src/App.jsx
@@ -1,33 +1,102 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import Signup from './pages/Signup'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Navbar from './components/Navbar'
+import Profile from './pages/Profile'
+import Blog from './pages/Blog'
+import CreateBlog from './pages/CreateBlog'
+import Dashboard from './pages/Dashboard'
+import YourBlog from './pages/YourBlog'
+import BlogView from './pages/BlogView'
+import Footer from './components/Footer'
+import About from './pages/About'
+import Comments from './pages/Comments'
+import UpdateBlog from './pages/UpdateBlog'
+import ProtectedRoute from './components/ProtectedRoute'
+import SearchList from './pages/SearchList'
 
-function App() {
-  const [count, setCount] = useState(0)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <><Navbar/><Home /><Footer/></>
+  },
+  {
+    path: "/blogs",
+    element: <><Navbar/><Blog /><Footer/></>
+  },
+  {
+    path: "/about",
+    element: <><Navbar/><About /><Footer/></>
+  },
+  {
+    path: "/search",
+    element: <><Navbar/><SearchList/><Footer/></>
+  },
+  {
+    path: "/blogs/:blogId",
+    element: <><Navbar/><ProtectedRoute><BlogView /></ProtectedRoute></>
+  },
+  {
+    path: "/write-blog",
+    element: <><Navbar/><CreateBlog /></>
+  },
+ 
+  {
+    path: "/profile",
+    element: <><Navbar/><Profile /></>
+  },
+  // {
+  //   path: "write-blog/:blogId",
+  //       element: <><Navbar/><CreateBlog /></>
+  // },
+  // {
+  //   path: "/dashboard",
+  //   element: <><Navbar/><Dashboard /></>
+  // },
+  {
+    path:"/dashboard",
+    element: <><Navbar/><ProtectedRoute><Dashboard/></ProtectedRoute></>,
+    children:[
+      {
+        path: "write-blog",
+        element:<><CreateBlog/></>
+      },
+      {
+        path: "write-blog/:blogId",
+        element: <><UpdateBlog /></>
+      },
+      {
+        path: "your-blog",
+        element:<YourBlog/>
+      },
+      {
+        path: "comments",
+        element:<Comments/>
+      },
+      {
+        path: "profile",
+        element:<Profile/>
+      },
+      
+      
+    ]
+   },
+  {
+    path: "/signup",
+    element: <><Navbar/><Signup /></> 
+  },
+  {
+    path: "/login",
+    element: <><Navbar/><Login /></>
+  },
+])
 
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
   )
 }
